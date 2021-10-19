916f5eaf939d67a1ff1f53f091dcbe82ee71188d
rammhapasekar <mhapasekar.ram@gmail.com>
2021-10-19 12:18:11 +0530

    Initial Commit

---
+++ Memorize_New/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  Memorize_New
+//
+//  Created by Ram Mhapasekar on 18/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🛻", "🚚", "🚛", "🚜", "🚲", "🛵", "🏍", "🛺", "🚝", "✈️", "🚤","🚂", "🚀", "🚁"]
+  @State var emojiCount = 5
+  
+  var body: some View {
+    VStack{
+      ScrollView{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+          ForEach(emojis[0..<emojiCount], id: \.self){ emoji in
+            CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+            
+          }
+        }
+      }
+      .foregroundColor(.red)
+      
+      Spacer()
+      
+      HStack{
+        remove
+        Spacer()
+        add
+      }
+      .font(.largeTitle)
+      .padding(.horizontal)
+    }
+    .padding(.horizontal)
+    
+  }
+  
+  var remove: some View {
+    Button{
+      if emojiCount > 1 {
+        emojiCount -= 1
+      }
+    } label: {
+      Image(systemName: "minus.circle")
+    }
+  }
+  
+  var add: some View {
+    Button{
+      if emojiCount < emojis.count {
+        emojiCount += 1
+      }
+    } label: {
+      Image(systemName: "plus.circle")
+    }
+  }
+}
+
+struct CardView: View{
+  var content: String
+  @State var isFaceUp: Bool = true
+  
+  var body: some View{
+    ZStack(alignment: .center){
+      let shape = RoundedRectangle(cornerRadius: 20.0)
+      if isFaceUp{
+        shape
+          .fill()
+          .foregroundColor(.white)
+        shape
+          .strokeBorder(lineWidth: 3)
+        Text(content)
+          .font(.largeTitle)
+      }
+      else{
+        shape
+          .fill()
+      }
+    }
+    .onTapGesture {
+      isFaceUp = !isFaceUp
+    }
+  }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+      .preferredColorScheme(.light)
+    ContentView()
+      .preferredColorScheme(.dark)
+  }
+}
---
+++ Memorize_New/Memorize_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_NewApp.swift
+//  Memorize_New
+//
+//  Created by Ram Mhapasekar on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
